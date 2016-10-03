9216425f9c36cdf7d42aa6e15c0b6f4a4b916b1f
lhcbernardes <lhcb@a.recife.ifpe.edu.br>
2016-10-03 13:26:33 -0300

    Update WordCount.java

--- test/teste/WordCount.java
+++ test/teste/WordCount.java
@@ -1,28 +1,46 @@
-
 package teste;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
+public class WordCount
+{
 
-public class WordCount {
     Map<String, Integer> counter = new HashMap<String, Integer>();
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException
+    {
         new WordCount().count();
     }
 
-    private void count() {
-        String string = "Hoje em dia, é necessário ser esperto. O nosso dia a dia é complicado.";
+    private void count() throws FileNotFoundException, IOException
+    {
+        URL url = new URL("https://jorgemachicado.blogspot.com.br/2010/07/cmt.html");
 
-        StringTokenizer token = new StringTokenizer(string, " .,?:"); //caracateres que não interessam
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String string = new String();
+        String linha;
+        /**
+         * while ((linha = in.readLine()) != null) { System.out.println(linha);
+         * }
+         */
+        while ((linha = in.readLine()) != null)
+        {
+            string = string + linha;
+        }
 
-        while (token.hasMoreTokens()) {
+        StringTokenizer token = new StringTokenizer(string, " /-=+{}()[]<>.,?:"); //caracateres que nÃ£o interessam
+
+        while (token.hasMoreTokens())
+        {
             String s = token.nextToken();
             count(s);
             System.out.println(s);
@@ -32,14 +50,18 @@ public class WordCount {
         print(string);
     }
 
-    private void count(String s) {
+    private void count(String s)
+    {
         Integer i = this.counter.get(s);
         this.counter.put(s, i == null ? 0 : ++i);
     }
 
-    private void print(String s) {
-        for (Entry<String, Integer> e : this.counter.entrySet()) {
-            if (e.getValue() > 0) {
+    private void print(String s)
+    {
+        for (Entry<String, Integer> e : this.counter.entrySet())
+        {
+            if (e.getValue() > 0)
+            {
                 s = s.replaceAll(e.getKey(), String.format("<b>%s</b>", e.getKey()));
             }
         }
